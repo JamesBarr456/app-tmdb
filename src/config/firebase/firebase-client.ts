603c173f1500927c9c52bf9f05691a139bfb17ca
Santiago barreto <94871122+JamesBarr456@users.noreply.github.com>
2025-05-20 09:59:33 -0300

    Update firebase-client.ts

--- src/config/firebase/firebase-client.ts
+++ src/config/firebase/firebase-client.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(firebaseConfig)
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
